dc2524ca84b6a5aaa331560916f04435ff53adb6
johnybas <johnbaswork@gmail.com>
2024-01-30 16:58:47 +0530

    cart page error fix

--- src/app/checkout/cart/page.tsx
+++ src/app/checkout/cart/page.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import React from 'react';
 import {
   CheckIcon,
@@ -9,44 +11,6 @@ import MainLayout from '@/components/layout/MainLayout';
 import Link from 'next/link';
 import { useCartStore } from '@/store/cartStore';
 
-// const products = [
-//   {
-//     id: 1,
-//     name: 'Basic Tee',
-//     href: '#',
-//     price: '$32.00',
-//     color: 'Sienna',
-//     inStock: true,
-//     size: 'Large',
-//     imageSrc:
-//       'https://tailwindui.com/img/ecommerce-images/shopping-cart-page-01-product-01.jpg',
-//     imageAlt: "Front of men's Basic Tee in sienna.",
-//   },
-//   {
-//     id: 2,
-//     name: 'Basic Tee',
-//     href: '#',
-//     price: '$32.00',
-//     color: 'Black',
-//     inStock: false,
-//     leadTime: '3–4 weeks',
-//     size: 'Large',
-//     imageSrc:
-//       'https://tailwindui.com/img/ecommerce-images/shopping-cart-page-01-product-02.jpg',
-//     imageAlt: "Front of men's Basic Tee in black.",
-//   },
-//   {
-//     id: 3,
-//     name: 'Nomad Tumbler',
-//     href: '#',
-//     price: '$35.00',
-//     color: 'White',
-//     inStock: true,
-//     imageSrc:
-//       'https://tailwindui.com/img/ecommerce-images/shopping-cart-page-01-product-03.jpg',
-//     imageAlt: 'Insulated bottle with white base and black snap lid.',
-//   },
-// ];
 const CartPage = () => {
   const { cart } = useCartStore();
 
