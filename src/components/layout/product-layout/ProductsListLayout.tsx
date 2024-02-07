66020080b29cff97cdaff2b1bd525712f5dfab52
johnybas <johnbaswork@gmail.com>
2024-02-07 17:29:31 +0530

    auth status and signin

--- src/components/layout/product-layout/ProductsListLayout.tsx
+++ src/components/layout/product-layout/ProductsListLayout.tsx
@@ -7,15 +7,9 @@ import {
   MinusIcon,
   Squares2X2Icon,
 } from '@heroicons/react/24/outline';
-import React, { Children, FormEvent, Fragment, Suspense } from 'react';
+import React, { Fragment } from 'react';
 import { filters, sortOptions, subCategories } from '@/constants/filters';
-import {
-  getCategories,
-  getProducts,
-} from '@/app/merchant/[merchant_id]/products-listing/fetcher';
 
-import { CategoryType } from '@/types/products';
-import FilterButton from './FilterButton';
 import MobileFilterMenu from './MobileFilterMenu';
 import SortFilterMenu, { SortOptions } from './SortFilterMenu';
 import { cn } from '@/lib/utils';
@@ -33,27 +27,70 @@ export type ProductListingParamsType = {
 
 const ProductsListLayout = ({
   children,
-  categories,
-  params,
-}: ProductListingParamsType & {
+  title = 'New Arrivals',
+  description,
+}: {
   children: React.ReactNode;
   categories: CategoryType[];
 }) => {
-  // const { data, isLoading, error } = useSwr('categories', () =>
-  // getCategories()
-  // );
-  // const categories: CategoryType[] = data;
-
   return (
     <div>
       {/* Mobile filter dialog */}
       <MobileFilterMenu />
 
       <main className='mx-auto mt-8 max-w-7xl px-4 sm:px-6 lg:px-8'>
+        <div className='pb-2'>
+          <h1 className='text-4xl font-bold tracking-tight text-gray-900'>
+            {title}
+          </h1>
+          <p className='mt-4 text-base text-gray-500'>{description}</p>
+        </div>
         <div className='flex items-baseline justify-end border-gray-200 pb-0 pt-4'>
           <div className='flex items-center'>
-            {/* Sort filter menu */}
-            <SortFilterMenu sortBy={params.sortBy || 'price-asc'} />
+            <Menu as='div' className='relative inline-block text-left'>
+              <div>
+                <Menu.Button className='group inline-flex justify-center text-sm font-medium text-gray-700 hover:text-gray-900'>
+                  Sort
+                  <ChevronDownIcon
+                    className='-mr-1 ml-1 h-5 w-5 flex-shrink-0 text-gray-400 group-hover:text-gray-500'
+                    aria-hidden='true'
+                  />
+                </Menu.Button>
+              </div>
+
+              <Transition
+                as={Fragment}
+                enter='transition ease-out duration-100'
+                enterFrom='transform opacity-0 scale-95'
+                enterTo='transform opacity-100 scale-100'
+                leave='transition ease-in duration-75'
+                leaveFrom='transform opacity-100 scale-100'
+                leaveTo='transform opacity-0 scale-95'
+              >
+                <Menu.Items className='absolute right-0 z-10 mt-2 w-40 origin-top-right rounded-md bg-white shadow-2xl ring-1 ring-black ring-opacity-5 focus:outline-none'>
+                  <div className='py-1'>
+                    {sortOptions.map((option) => (
+                      <Menu.Item key={option.name}>
+                        {({ active }) => (
+                          <a
+                            href={option.href}
+                            className={cn(
+                              option.current
+                                ? 'font-medium text-gray-900'
+                                : 'text-gray-500',
+                              active ? 'bg-gray-100' : '',
+                              'block px-4 py-2 text-sm'
+                            )}
+                          >
+                            {option.name}
+                          </a>
+                        )}
+                      </Menu.Item>
+                    ))}
+                  </div>
+                </Menu.Items>
+              </Transition>
+            </Menu>
 
             <button
               type='button'
@@ -83,24 +120,79 @@ const ProductsListLayout = ({
 
           <div className='grid grid-cols-1 gap-x-8 gap-y-10 lg:grid-cols-4'>
             {/* Filters */}
-            <div className='hidden lg:block'>
+            <form className='hidden lg:block'>
               <div className='lg:sticky lg:top-16'>
-                <FilterListWrapper title='Categories'>
-                  {categories &&
-                    categories.map((category, categoryIdx) => (
-                      <div
-                        key={category.title + categoryIdx}
-                        className='flex items-center'
-                      >
-                        {/* filter button with action */}
-                        <FilterButton category={category} />
-                      </div>
-                    ))}
+                <h3 className='sr-only'>Categories</h3>
+                <ul
+                  role='list'
+                  className='space-y-4 border-b border-gray-200 pb-6 text-sm font-medium text-gray-900'
+                >
+                  {subCategories.map((category) => (
+                    <li key={category.name}>
+                      <a href={category.href}>{category.name}</a>
+                    </li>
+                  ))}
+                </ul>
 
-                  {/* <ProductCategories categories={categories} /> */}
-                </FilterListWrapper>
+                {filters.map((section) => (
+                  <Disclosure
+                    as='div'
+                    key={section.id}
+                    className='border-b border-gray-200 py-6'
+                  >
+                    {({ open }) => (
+                      <>
+                        <h3 className='-my-3 flow-root'>
+                          <Disclosure.Button className='flex w-full items-center justify-between bg-white py-3 text-sm text-gray-400 hover:text-gray-500'>
+                            <span className='font-medium text-gray-900'>
+                              {section.name}
+                            </span>
+                            <span className='ml-6 flex items-center'>
+                              {open ? (
+                                <MinusIcon
+                                  className='h-5 w-5'
+                                  aria-hidden='true'
+                                />
+                              ) : (
+                                <PlusIcon
+                                  className='h-5 w-5'
+                                  aria-hidden='true'
+                                />
+                              )}
+                            </span>
+                          </Disclosure.Button>
+                        </h3>
+                        <Disclosure.Panel className='pt-6'>
+                          <div className='space-y-4'>
+                            {section.options.map((option, optionIdx) => (
+                              <div
+                                key={option.value}
+                                className='flex items-center'
+                              >
+                                <input
+                                  id={`filter-${section.id}-${optionIdx}`}
+                                  name={`${section.id}[]`}
+                                  defaultValue={option.value}
+                                  type='checkbox'
+                                  defaultChecked={option.checked}
+                                  className='h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500'
+                                />
+                                <label
+                                  htmlFor={`filter-${section.id}-${optionIdx}`}
+                                  className='ml-3 text-sm text-gray-600'
+                                >
+                                  {option.label}
+                                </label>
+                              </div>
+                            ))}
+                          </div>
+                        </Disclosure.Panel>
+                      </>
+                    )}
+                  </Disclosure>
+                ))}
               </div>
-            </div>
+            </form>
 
             {/* Product grid */}
             <div className='lg:col-span-3'>{children}</div>
